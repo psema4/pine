d35e53936cbf4c21b9bd27e740173fc5827b4f53
Jeremy Kahn <jeremyckahn@gmail.com>
2012-07-29 14:26:19 -0700

    Changes script for installing Node.

--- util/prep_raspbian_for_pine.sh
+++ util/prep_raspbian_for_pine.sh
@@ -20,7 +20,7 @@ apt-get update
 
 ####### GET DEPENDENCIES
 
-apt-get install curl xorg mate-core mate-desktop-environment
+apt-get install curl xorg mate-core mate-desktop-environment nodejs
 
 # Install rpi-update (https://github.com/Hexxeh/rpi-update/)
 wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
@@ -30,34 +30,6 @@ wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
 
 # Install Hexxeh's Chromium. (http://hexxeh.net/?p=328117859)
 bash <(curl -sL http://goo.gl/go5yx)
-
-
-####### INSTALL NODE
-
-# Install Git and Node. Adapted from:
-#   http://www.convery.me.uk/blog/install-node-js-v0-8-2-on-raspbian/
-sudo apt-get install git-core build-essential
-
-cd ~/
-
-# Download the source and patch it
-git clone git://github.com/gflarity/node_pi.git
-git clone git://github.com/joyent/node.git
-cd node
-git checkout origin/v0.8.2-release -b v0.8.2-release
-git apply --stat ../node_pi/v0.8.2-release-raspberrypi.patch
-
-#Set the flags needed for compilation
-export GYP_DEFINES="armv7=0"
-export CXXFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard -DUSE_EABI_HARDFLOAT'
-export CCFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard -DUSE_EABI_HARDFLOAT'
-
-# Configure the compilation
-./configure --shared-openssl --without-snapshot
-
-# Make & install
-make
-sudo GYP_DEFINES="armv7=0" CXXFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard -DUSE_EABI_HARDFLOAT' CCFLAGS='-march=armv6 -mfpu=vfp -mfloat-abi=hard -DUSE_EABI_HARDFLOAT' make install
 
 
 ####### PERFORMANCE HACKS
