eda2843e5da7fcf43d8e66894a29c214ee72658f
Jeremy Kahn <jeremyckahn@gmail.com>
2012-08-21 21:33:00 -0700

    Adds Node test server for ui/.

---
+++ ui/README.md
@@ -0,0 +1,10 @@
+# UI
+
+To play with the Pine UI the UI in your desktop browser, you need to start the
+test server:
+
+````
+$: node test-server.js
+````
+
+And navigate to `http://localhost:5000/` in your browser.
---
+++ ui/test-server.js
@@ -0,0 +1,8 @@
+var static = require('node-static');
+var file = new(static.Server)('./');
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  });
+}).listen(5000);
