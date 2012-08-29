8eca4d5db8b7da2a53d15afb71ca74e5785cbbb3
Jeremy Kahn <jeremyckahn@gmail.com>
2012-08-28 23:05:32 -0700

    Adds a benchmark for divs that are rotated by JavaScript.
    Squashed commit of the following:
    
    commit 185194d5a2cc89fc3c58875960b68f26f2a8ff9f
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 23:04:06 2012 -0700
    
        Hooks up the Squares benchmark to the main UI.
    
    commit 677d678b1edb41c085e02fecca17cdc8feff94cb
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 22:58:35 2012 -0700
    
        Adds square removal functionality.
    
    commit 44bf1fdaa6b3990e3e0b013b2845489ccab3d31e
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 22:51:40 2012 -0700
    
        Adds a square counter.
    
    commit 6586d4a9fb6778c4d3ac59846787d66047cc2699
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 20:52:13 2012 -0700
    
        Makes rotators rotate.
    
    commit a45a62f061abef29dcbde4dc0cf216e4aaf15bd0
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 20:36:32 2012 -0700
    
        Adds divs and places them randomly.
    
    commit 510f893c2e46748e26cfb6a7b5d220de1fa9962d
    Author: Jeremy Kahn <jeremyckahn@gmail.com>
    Date:   Tue Aug 28 20:01:23 2012 -0700
    
        Implements div appending to rotation benchmark.

--- ui/benchmark/js/src/benchmark.js
+++ ui/benchmark/js/src/benchmark.js
@@ -3,12 +3,35 @@
   'use strict';
 
 
+  var onTick = function () {};
+  var input = global.input = new pine.Input();
+
+  var docEl = document.documentElement;
   var testName = document.getElementById('test-name');
   var framerateOutput = document.getElementById('framerate-output');
+  var squareCountOutput = document.getElementById('square-count-output');
+  var squareCountOutputDD = squareCountOutput.querySelector('dd');
+  var scratch = document.getElementById('scratch');
+
   var framerate = new Framerate();
   framerate.onUpdate = function (rate) {
     framerateOutput.textContent = rate;
   };
+
+  // Set up event listeners if we are running the game directly (not via the
+  // Pine interface).
+  if (!pine.env.isSandboxed) {
+    ['keydown', 'keyup'].forEach(function (eventName) {
+      docEl.addEventListener(eventName, function (evt) {
+        input[eventName](evt.which);
+        evt.preventDefault();
+      });
+    });
+  }
+
+
+  // HELPER FUNCTIONS
+  //
 
 
   /**
@@ -31,18 +54,104 @@
 
   function tick () {
     webkitRequestAnimationFrame(tick);
+    onTick();
     framerate.tick();
+  }
+
+
+  /**
+   * @return {Array.<HTMLElement>}
+   */
+  function getChildren (parent) {
+    var children = parent.children;
+    return Array.prototype.slice.call(children, 0);
+  }
+
+
+  /**
+   * @return {string}
+   */
+  function getRandomColorString () {
+    return 'rgb('
+        + Math.floor(Math.random() * 256) + ','
+        + Math.floor(Math.random() * 256) + ','
+        + Math.floor(Math.random() * 256) + ')';
+  }
+
+
+  // BENCHMARK HELPERS
+  //
+
+
+  function  updateSquareCount () {
+    squareCountOutputDD.textContent = getChildren(scratch).length;
+  }
+
+
+  function addRotatingDiv () {
+    var div = document.createElement('div');
+    div.classList.add('js-rotator');
+    div.style.backgroundColor = getRandomColorString();
+    div.style.left = (Math.random() * 90) + '%';
+    div.style.top = (Math.random() * 90) + '%';
+    div.style.webkitTransform = 'rotate(0deg)';
+    scratch.appendChild(div);
+    var childCount = getChildren(scratch).length;
+    updateSquareCount();
+  }
+
+
+  function removeRotatingDiv () {
+    var children = getChildren(scratch);
+
+    if (children.length) {
+      var lastChild = children[children.length - 1];
+      scratch.removeChild(lastChild);
+      updateSquareCount();
+    }
+  }
+
+
+  function rotatingDivsBenchmarkOnTick () {
+    var childrenArr = getChildren(scratch);
+    childrenArr.forEach(function (child) {
+      var currentTransform = child.style.webkitTransform;
+      var currentRotation = +currentTransform.match(/\d+/)[0];
+      var newRotation = ++currentRotation % 360;
+      child.style.webkitTransform = 'rotate(' + newRotation + 'deg)';
+    });
+  }
+
+
+  // BENCHMARKS
+  //
+
+  function runRotatingDivsBenchmark () {
+    input.onKeydown(function (keycode) {
+      if (keycode === 32) { // space
+        addRotatingDiv();
+      } else if (keycode === 8) { // backspace
+        removeRotatingDiv();
+      }
+    });
+
+    onTick = rotatingDivsBenchmarkOnTick;
+    squareCountOutput.style.display = 'block';
   }
 
 
   // Test init logic
   (function () {
     var queryParams = getQueryParams();
+    var benchmark = queryParams.benchmark;
     tick();
 
     // This is the default test.
-    if (!queryParams.benchmark || queryParams.benchmark === 'idle') {
+    if (!benchmark || benchmark === 'idle') {
       testName.textContent += ' - Idle';
+    } else if (benchmark === 'rotating-divs') {
+      testName.textContent += ' - Rotating Divs';
+      runRotatingDivsBenchmark();
     }
   } ());
 
