674a57f6e997d0518409c313dc29dcbc47c9758a
Jeremy Kahn <jeremyckahn@gmail.com>
2012-09-06 21:39:55 -0700

    Fixes invisible character issues with kill_chrome.sh

--- auto-pine/defaults/stage-2/home/pine-user/kill_chrome.sh
+++ auto-pine/defaults/stage-2/home/pine-user/kill_chrome.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # Kills Chrome
 # http://www.commandlinefu.com/commands/view/1138/ps-ef-grep-process-grep-v-grep-awk-print-2-xargs-kill-9
-ps aux | grep xinit.*chrome | awk '{print $2}' | xargs kill
+ps aux | grep xinit.*chrome | awk '{print $2}'| xargs kill;
