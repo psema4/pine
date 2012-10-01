bc61624f410bc315dc2fd2d9b7e18854066fe9af
Jeremy Kahn <jeremyckahn@gmail.com>
2012-09-30 18:24:41 -0700

    Replaces some strange characters resulting from a copy/paste.

--- ui/js/plugin/v-keys-helper.js
+++ ui/js/plugin/v-keys-helper.js
@@ -6,18 +6,18 @@ define(['exports'], function (vKeysHelper) {
   // This function was unabashedly copy/pasted from:
   // http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
   $.fn.setCursorPosition = function(pos) {
-    if ($(this).get(0).setSelectionRange) {
-      $(this).get(0).setSelectionRange(pos, pos);
-    } else if ($(this).get(0).createTextRange) {
-      var range = $(this).get(0).createTextRange();
-      range.collapse(true);
-      range.moveEnd('character', pos);
-      range.moveStart('character', pos);
-      range.select();
-    }
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
 
     return this;
-  }
+  }
 
 
   /** @type {string} */
