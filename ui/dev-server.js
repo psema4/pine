272e7bb721d0524b1728c87d6f7423c56c44a862
Jeremy Kahn <jeremyckahn@gmail.com>
2012-11-03 14:41:43 -0700

    Big file reorganization.
    Common JavaScript and CSS files have been moved to ui/public. Moved
    "game" packages to ui/packages. api/ symlinks have been removed.  src
    links have been updated across all files.

--- ui/dev-server.js
+++ ui/dev-server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var server = express();
 
 server.use(express.static(__dirname + '/'));
+server.use('/public', express.static(__dirname + '/public'));
+
+//TODO(jeremyckahn): Serving the api/ directory like this is a little silly.  I
+//tried to symlink it in the public/ directory, but it wasn't working for
+//whatever reason.  This might be a limitation of Express.  Try to straighten
+//this out.
+server.use('/api', express.static(__dirname + '/../api'));
 
 server.listen(5000);
 
