08312f0e4530c472217beae1f9863440aaaa56b0
Jeremy Kahn <jeremyckahn@gmail.com>
2012-11-03 14:10:41 -0700

    Remove call to Express#configure.

--- ui/dev-server.js
+++ ui/dev-server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var server = express();
 
-server.configure(function(){
-  server.use('/', express.static(__dirname + '/'));
-});
+server.use(express.static(__dirname + '/'));
 
 server.listen(5000);
 
