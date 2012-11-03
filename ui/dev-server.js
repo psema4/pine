43209be358c18b051348cc76aa6f70a9dba616c6
Jeremy Kahn <jeremyckahn@gmail.com>
2012-11-03 13:36:07 -0700

    Switches to Express for serving files.

--- ui/dev-server.js
+++ ui/dev-server.js
@@ -1,11 +1,11 @@
-var static = require('node-static');
-var file = new(static.Server)('./');
+var express = require('express');
+var server = express();
 
-require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
-  });
-}).listen(5000);
+server.configure(function(){
+  server.use('/', express.static(__dirname + '/'));
+});
+
+server.listen(5000);
 
 console.log('Pine dev server is now listening on port 5000.');
 console.log('Press ctrl+c to kill the server.');
