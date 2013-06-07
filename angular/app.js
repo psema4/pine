2bef11c37f0d33c25f609a473f08bc87ffb15486
Scott Elcomb <psema4@gmail.com>
2013-06-07 14:35:12 -0400

    destroy dom refs on view change, try to force garbage collection

--- angular/app.js
+++ angular/app.js
@@ -16,9 +16,26 @@ testApp.config(function($routeProvider) {
     })
     .otherwise({redirectTo: '/Environment'});
 
-}).run(function($rootScope, $location, $window, Splash) {
+}).run(function($rootScope, $location, $templateCache, $window, Splash) {
     $rootScope.$on("$routeChangeStart", function(evt, next, current) {
+        // try to remove as many dom-refs as possible - particularly the reference to canvas, stored within Game.ctx [created in gameController.$scope.setup()]
+        for (var key in $window.Game) {
+            delete $window.Game[key];
+        };
         $window.Game = {};
+
+        // try to clear ng's template cache
+        //console.log('$templaceCach.info():', $templateCache.info());
+        $templateCache.removeAll();
+
+        // force garbage collection if available
+        //
+        //   note: chrome requires --js-flags=--expose-gc
+        //         eg. bash$ /usr/bin/google-chrome --js-flags=--expose-gc
+        //
+        if ('gc' in $window && typeof $window.gc == 'function') {
+            $window.gc();
+        }
     });
 
     $rootScope.$on("$routeChangeSuccess", function(evt, current, previous) {
